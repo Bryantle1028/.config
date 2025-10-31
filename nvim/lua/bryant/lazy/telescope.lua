fed3c9114e588e3908e06da8140bbe8607a2df21
Bryant Le <bryant@fernstone.com>
2025-10-31 12:33:49 -0700

    fix rigrep

--- nvim/lua/bryant/lazy/telescope.lua
+++ nvim/lua/bryant/lazy/telescope.lua
@@ -1,21 +1,33 @@
 return {
 	"nvim-telescope/telescope.nvim",
 
-	tag = "0.1.5",
+	branch = "0.1.x",
 
 	dependencies = {
 		"nvim-lua/plenary.nvim"
 	},
 
 	config = function()
-		require('telescope').setup({})
+		require('telescope').setup({
+			defaults = {
+				file_ignore_patterns = { "node_modules", ".git/" },
+				vimgrep_arguments = {
+					"/opt/homebrew/bin/rg",
+					"--color=never",
+					"--no-heading",
+					"--with-filename",
+					"--line-number",
+					"--column",
+					"--smart-case",
+				},
+			},
+		})
 
 		local builtin = require('telescope.builtin')
 		vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 		vim.keymap.set('n', '<leader>gf', builtin.git_files, {})
-		vim.keymap.set('n', '<leader>ps', function()
-			builtin.grep_string({ search = vim.fn.input("Grep > ") })
-		end)
+		vim.keymap.set('n', '<leader>ps', builtin.live_grep, {})
+		vim.keymap.set('n', '<leader>pw', builtin.grep_string, {}) -- search for word under cursor
 	end
 }
 
